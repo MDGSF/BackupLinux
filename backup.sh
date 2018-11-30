3b747263077a02c11c3af5dce13533fcc34a899a
huangjian <1342042894@qq.com>
2018-11-30 20:03:48 +0800

    modify backup.sh

--- backup.sh
+++ backup.sh
@@ -2,53 +2,86 @@
 
 HOME_BACKUP=$HOME/home_backup
 
-baisc() {
-  if [ ! -d "$HOME/home_backup" ];then
-    echo "$HOME/home_backup 文件夹不存在，创建文件夹"
-    mkdir $HOME/home_backup
+Backup_c1() {
+  if [ ! -d "$HOME_BACKUP/c1" ];then
+    echo "$HOME_BACKUP/c1 文件夹不存在，创建文件夹"
+    mkdir -p $HOME_BACKUP/c1
   fi
 
-  if [ ! -d "$HOME/home_backup/c1" ];then
-    echo "$HOME/home_backup/c1 文件夹不存在，创建文件夹"
-    mkdir -p $HOME/home_backup/c1
-  fi
-
-  cp -arf $HOME/backup.sh $HOME_BACKUP
-
-  cp -arf $HOME/.tmux.conf $HOME_BACKUP
-
-  cp -arf $HOME/note $HOME/home_backup
-  cp -arf $HOME/.ssh $HOME/home_backup/ssh_backup
-
-  cp -arf $HOME/local/bin/c1 $HOME/home_backup/c1/
-  cp -arf $HOME/.c1 $HOME/home_backup/c1/c1_backup
-  cp -arf $HOME/local/DevicesType $HOME/home_backup/
-
-  tar -zcvf home_backup`date +"%Y%m%d%H%M%S"`.tar.gz home_backup
-  rm -rf home_backup
+  cp -arf $HOME/local/bin/c1 $HOME_BACKUP/c1/
+  cp -arf $HOME/.c1 $HOME_BACKUP/c1/c1_backup
+  cp -arf $HOME/local/DevicesType $HOME_BACKUP/
 }
 
-vimBackup() {
-  if [ ! -d "$HOME/vim_backup" ];then
-    echo "$HOME/vim_backup 文件夹不存在，创建文件夹"
-    mkdir $HOME/vim_backup
+Restore_c1() {
+  echo ""
+}
+
+Backup_note() {
+  cp -arf $HOME/note $HOME_BACKUP/note_backup
+}
+Restore_note() {
+  cp -arf $HOME_BACKUP/note_backup $HOME/note
+}
+
+Backup_ssh() {
+  cp -arf $HOME/.ssh $HOME_BACKUP/ssh_backup
+}
+Restore_ssh() {
+  cp -arf $HOME_BACKUP/ssh_backup $HOME/.ssh
+}
+
+Backup_tmux() {
+  cp -arf $HOME/.tmux.conf $HOME_BACKUP/tmux.conf.backup
+}
+Restore_tmux() {
+  cp -arf $HOME_BACKUP/tmux.conf.backup $HOME/.tmux.conf
+}
+
+Backup_vim() {
+  VIM_BACKUP=$HOME_BACKUP
+  if [ ! -d "$VIM_BACKUP" ];then
+    echo "$VIM_BACKUP 文件夹不存在，创建文件夹"
+    mkdir -p $VIM_BACKUP
   fi
 
-  cp -arf .vimrc $HOME/vim_backup/vimrc_backup
-  cp -arf .vim $HOME/vim_backup/vim_backup
-  tar -zcvf vim_backup`date +"%Y%m%d%H%M%S"`.tar.gz vim_backup
-  rm -rf $HOME/vim_backup
+  cp -arf $HOME/.vimrc $VIM_BACKUP/vimrc_backup
+  cp -arf $HOME/.vim $VIM_BACKUP/vim_backup
+}
+
+api_baisc() {
+  if [ ! -d "$HOME_BACKUP" ];then
+    echo "$HOME_BACKUP 文件夹不存在，创建文件夹"
+    mkdir -p $HOME_BACKUP
+  fi
+
+  Backup_c1
+  Backup_note
+  Backup_ssh
+  Backup_tmux
+  Backup_vim
+
+  tar -zcvf home_backup`date +"%Y%m%d%H%M%S"`.tar.gz $HOME_BACKUP
+  rm -rf $HOME_BACKUP
+}
+
+api_Backup_development_environment() {
+  echo ""
 }
 
 case "$1" in
   'basic')
-    baisc
+    api_baisc
     ;;
   'vim')
-    vimBackup
+    ;;
+  'dev')
+    api_Backup_development_environment
     ;;
   *)
     echo "usage: $0 {basic|vim|restore}"
     exit 1
     ;;
 esac
+
+
